535763e2ce4f063e9a478f01a0c266792f75e8b4
HOME <home@Adams-MacBook-Pro.local>
2024-08-15 23:18:54 +0700

    add more sols

---
+++ 2004C.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC target("sse,sse2,sse3,ssse3,sse4,popcnt,abm,mmx,avx,avx2,fma")
+#pragma GCC optimize("unroll-loops")
+
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+#define int long long 
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = (ll)(2e18);
+
+void sol()
+{
+    int n, k; cin >> n >> k;
+    v64 a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+
+    sort(all(a), greater<int>());
+    for (int i=1; i<n; i+=2)
+    {
+        if (k <= 0) break;
+        int mid = min(k, a[i-1]-a[i]);
+        a[i] += mid;
+        k -= mid;
+    }
+
+    //debug
+    //for (int i=0; i<n; i++) cout << a[i] << " ";
+    //cout << el;
+    int ans = 0;
+    for (int i=1; i<n; i+=2) ans += (a[i-1]-a[i]);
+    if (n%2) ans += a[n-1];
+    cout << ans << el;
+}
+
+int32_t main()
+{
+    io();
+    int t; cin >> t;
+    while (t--) sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
