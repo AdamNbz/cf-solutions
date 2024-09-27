486a9bda02146fccde306c7e54f2d21ba5d969b6
HOME <home@Adams-MacBook-Pro.local>
2024-09-27 22:53:11 +0700

    updates

---
+++ 2019A.cpp
@@ -0,0 +1,76 @@
+/*
+Author : Nguyen Vo Ngoc Bao
+School : University of Information Technology, VNU-HCM
+*/
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+#define int long long
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+template<typename T> void ckmin(T& x, T y) {if (x>y) x = y;}
+template<typename T> void ckmax(T& x, T y) {if (x<y) x = y;}
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = (ll)(2e18);
+
+int n, i = 1;
+v32 a, odd, even;
+
+void sol()
+{
+    cin >> n;
+    a.resize(n+1);
+    int mx = 1;
+    for (int i=1; i<=n; i++)
+    {
+        cin >> a[i];
+        if (a[mx] <= a[i]) 
+            if (a[i] > a[mx] || (mx%2 != n%2)) mx = i;
+    }
+    cout << a[mx] + (n%2 == mx%2 ? (n+1)/2 : n/2) << el;
+}
+
+int32_t main()
+{
+    io();
+    int t; cin >> t;
+    while (t--) sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
