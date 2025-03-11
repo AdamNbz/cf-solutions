a4a3735c304ec79075b73f8e3f7f094365c853bf
Nguyễn Võ Ngọc Bảo <contact.adamnguyen@gmail.com>
2025-03-12 00:20:58 +0700

    sigma

--- 2074G.cpp
+++ 2074G.cpp
@@ -11,28 +11,25 @@ template<typename T> void ckmax(T& x, T y) {if (x<y) x = y;}
 
 const ll mod = (ll)(1e9+7);
 const ll inf = numeric_limits<ll>::max();
-const int mxN = 401;
-
-ll dp[mxN][mxN], a[mxN];
 
 void skibidi()
 {
     int n; cin >> n;
-    for (int i=1; i<=n; i++) cin >> a[i];
-    for (int i=1; i<=n; i++) for (int j=i; j<=n; j++) dp[i][j] = 0;
-
-    for (int i=3; i<=n; i++)
+    vector<ll> a(n);
+    for (int i=0; i<n; i++) cin >> a[i];
+    vector<vector<ll>> dp(n, vector<ll>(n));
+    for (int i=n-1; i>=0; i--)
     {
-        for (int j=1; j<=n+1-i; j++)
+        for (int j=i+1; j<n; j++)
         {
-            int k = j+i-1;
-            ckmax(dp[j][k], dp[j+1][k]);
-            ckmax(dp[j][k], dp[j][k-1]);
-            for (int m=j+1; m<k; m++) ckmax(dp[j][k], dp[j+1][m-1]+dp[m+1][k-1]+a[j]*a[m]*a[k]);
+            for (int k=i; k<j; k++)
+                ckmax(dp[i][j], dp[i][k] + dp[k+1][j]);
+            for (int k=i+1; k<j; k++)
+                ckmax(dp[i][j], dp[i+1][k-1] + dp[k+1][j-1] + a[i]*a[j]*a[k]);
         }
     }
 
-    cout << dp[1][n] << '\n';
+    cout << dp[0][n-1] << '\n';
 }
 
 signed main()
