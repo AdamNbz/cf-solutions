5777861b812c4e4c1abfa90b4583343684892687
Nguyễn Võ Ngọc Bảo <contact.adamnguyen@gmail.com>
2025-04-07 09:37:51 +0700

    practice

---
+++ 25D.cpp
@@ -0,0 +1,117 @@
+/*
+Author : Nguyen Vo Ngoc Bao
+School : University of Information Technology, VNU-HCM
+*/
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#ifndef ONLINE_JUDGE
+   #if defined(__APPLE__)
+      #include </Users/home/Documents/nbz_lib/debug.h>
+   #elif defined(_WIN32) || defined(_WIN64)
+      #include <C:/Users/Admin/OneDrive/Documents/nbz_lib/debug.h>
+   #endif
+#else
+   #define dbg(...) 
+#endif
+
+#define nbzzz signed main
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+template<typename T> void ckmin(T& x, T y) {if (x>y) x = y;}
+template<typename T> void ckmax(T& x, T y) {if (x<y) x = y;}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+int parity(ull mask) { return __builtin_parityll(mask); }
+
+const ll mod = (ll)(1e9+7);
+const ll inf = numeric_limits<ll>::max();
+
+struct dsu
+{
+    int n;
+    vector<int> par, s, r;
+    dsu(int _n): n(_n), par(n), s(n, 1), r(n, 0) { iota(all(par), 0); }
+    int get(int u) { return u == par[u] ? u : par[u] = get(par[u]); }
+    int disjointSet() { return n; }
+    int sizes(int u) { return s[get(u)]; }
+    bool same(int u, int v) { return get(u) == get(v); }
+    bool unite(int u, int v) 
+    {
+        u = get(u), v = get(v);
+        if (u == v) return 0;
+        if (r[u] < r[v]) swap(u, v);
+        par[v] = u;
+        r[u] += r[u] == r[v];
+        s[u] += s[v];
+        --n;
+        return 1;
+    }
+};
+
+void sol()
+{
+    int n; cin >> n;
+    vp32 city(n-1); 
+    for (auto &[x, y]: city) 
+    {
+        cin >> x >> y;
+        --x, --y;
+    }
+    dsu d(n);
+    list<p32> ans;
+    int cnt = 0;
+    for (auto &[x, y]: city) if (!d.unite(x, y)) ans.pb({x, y}), cnt++;
+
+    cout << cnt << el;
+    for (int i=1; i<n; i++)
+    {
+        if (d.unite(0, i))
+        {
+            cout << ans.front().fi+1 << " " << ans.front().se+1 << " " << 1 << " " << i+1 << el;
+            ans.pop_front();
+        }
+    }
+}
+
+nbzzz()
+{
+    io();
+    sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
