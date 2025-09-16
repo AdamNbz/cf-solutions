3f8339868f7b327854046408239d96e914165122
Nguyễn Võ Ngọc Bảo <contact.adamnguyen@gmail.com>
2025-09-16 08:43:05 +0700

    practice edu round

---
+++ 2144C.cpp
@@ -0,0 +1,88 @@
+/*
+Author : Nguyen Vo Ngoc Bao
+School : University of Information Technology, VNU-HCM
+*/
+#include <bits/stdc++.h>
+
+#pragma GCC optimize("Ofast")
+#pragma GCC optimize("unroll-loops")
+
+#ifndef ONLINE_JUDGE
+   #if defined(__APPLE__)
+      #include </Users/home/Documents/nbz_lib/debug.h>
+   #elif defined(_WIN32) || defined(_WIN64)
+      #include <C:/Users/Admin/OneDrive/Documents/nbz_lib/debug.h>
+   #endif
+#else
+   #define dbg(...) 
+#endif
+
+#define nbzzz signed main
+#define fi first
+#define se second
+#define el '\n'
+#define pb push_back
+#define eb emplace_back
+#define io() ios_base::sync_with_stdio(0); cin.tie(NULL); cout.tie(NULL)
+#define all(x) (x).begin(), (x).end()
+#define sz(x) ((ll)(x).size())
+#define mask(i) (1LL << (i))
+#define getbit(mask, i) (((mask) >> (i)) & 1)
+
+using namespace std;
+
+typedef unsigned long long ull;
+typedef long long ll;
+typedef long double ld;
+typedef pair<int, int> p32;
+typedef pair<ll, ll> p64;
+typedef pair<double, double> pdd;
+typedef vector<int> v32;
+typedef vector<vector<int> > vv32;
+typedef vector<ll> v64;
+typedef vector<vector<ll> > vv64;
+typedef vector<p32> vp32;
+typedef vector<vector<p32> > vvp32;
+typedef vector<p64> vp64;
+typedef vector<vector<p64> > vvp64;
+typedef vector<bool> vb;
+typedef vector<vector<bool> > vvb;
+typedef vector<string> vs;
+typedef vector<vs > vvs;
+
+template<typename T> void ckmin(T& x, T y) {if (x>y) x = y;}
+template<typename T> void ckmax(T& x, T y) {if (x<y) x = y;}
+mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+
+int pop_cnt(ll mask) { return __builtin_popcountll(mask); }
+int ctz(ull mask) { return __builtin_ctzll(mask); }
+int logOf(ull mask) { return 63 - __builtin_clzll(mask); }
+int parity(ull mask) { return __builtin_parityll(mask); }
+
+const ll mod = 998244353;
+const ll inf = numeric_limits<ll>::max();
+
+void sol()
+{
+    int n; cin >> n;
+    v32 a(n+1), b(n+1); for (int i=1; i<=n; i++) cin >> a[i]; for (int i=1; i<=n; i++) cin >> b[i];
+
+    vv32 dp(n+1, v32(2));
+    dp[1][0] = dp[1][1] = 1;
+    for (int i=2; i<=n; i++)
+    {
+        if (a[i-1] <= a[i] && b[i-1] <= b[i]) dp[i][0] = (dp[i][0]+dp[i-1][0])%mod;
+        if (b[i-1] <= a[i] && a[i-1] <= b[i]) dp[i][0] = (dp[i][0]+dp[i-1][1])%mod;
+        if (a[i-1] <= b[i] && b[i-1] <= a[i]) dp[i][1] = (dp[i][1]+dp[i-1][0])%mod;
+        if (b[i-1] <= b[i] && a[i-1] <= a[i]) dp[i][1] = (dp[i][1]+dp[i-1][1])%mod;
+    }
+    cout << (dp[n][0]+dp[n][1])%mod << el;
+}
+
+nbzzz()
+{
+    io();
+    int t; cin >> t;
+    while (t--) sol();
+    cerr << "\nTime elapsed: " << 1000*clock()/CLOCKS_PER_SEC << "ms\n";
+}
